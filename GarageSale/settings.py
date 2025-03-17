d145ec48e439f423252b48627aa2d4722ebf4304
tony Flury <anthony.flury@btinternet.com>
2025-03-17 13:48:38 +0000

    Formatting issue on Location page - temp fix

--- GarageSale/settings.py
+++ GarageSale/settings.py
@@ -44,10 +44,10 @@ DEBUG = debug.DEBUG if debug else False
 ALLOWED_HOSTS = hosts.ALLOWED_HOSTS
 INTERNAL_IPS = hosts.INTERNAL_IPS
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = 'static'
 MEDIA_ROOT = 'media'
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 
 # Application definition
 
